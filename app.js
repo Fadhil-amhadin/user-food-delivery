f51d5be6021457caf953ca6071970545282eb773
unknown <fadhilamhadin@gmail.com>
2022-01-15 22:17:38 +0700

    configure database

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', (req , res , next) => {
+app.use('/uploads', (req , res , next) => {
     const signature = req.get('signature');
     if (signature === "midasfooddelivery") {
       next();
