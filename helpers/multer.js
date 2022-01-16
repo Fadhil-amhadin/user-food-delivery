1612c72426c44fda845a69a1882b02a021afa9c6
unknown <fadhilamhadin@gmail.com>
2022-01-16 21:17:16 +0700

    configure database

--- helpers/multer.js
+++
@@ -1,7 +0,0 @@
-const multer = require ('multer')
-
-const storage = multer.memoryStorage()
-const upload = multer({storage: storage})
-const uploadSingle = upload.single('image')
-
-module.exports = uploadSingle
